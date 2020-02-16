d91cb762291677ffb63ec075c450a49ed6298284
Martin Reinecke <martin@mpa-garching.mpg.de>
2020-02-16 18:07:39 +0100

    more packaging

---
+++ pysharp/test/test.py
@@ -0,0 +1,43 @@
+import pysharp
+import numpy as np
+import math
+import pytest
+from numpy.testing import assert_equal, assert_allclose
+
+pmp = pytest.mark.parametrize
+
+@pmp('params', [(511, 511, 512, 1024),
+                (511, 2, 512, 5),
+                (511, 0, 512, 1)])
+def test_GL(params):
+    lmax, mmax, nlat, nlon = params
+    job = pysharp.sharpjob_d()
+    nalm = ((mmax+1)*(mmax+2))//2 + (mmax+1)*(lmax-mmax)
+    nalm_r = nalm*2-lmax-1
+    alm_r = np.random.uniform(-1., 1., nalm_r)
+    alm = np.empty(nalm, dtype=np.complex128)
+    alm[0:lmax+1] = alm_r[0:lmax+1]
+    alm[lmax+1:] = np.sqrt(0.5)*(alm_r[lmax+1::2] + 1j*alm_r[lmax+2::2])
+
+    job.set_triangular_alm_info(lmax, mmax)
+    job.set_Gauss_geometry(nlat, nlon)
+    alm2 = job.map2alm(job.alm2map(alm))
+    assert_allclose(alm, alm2)
+
+@pmp('params', [(511, 511, 1024, 1024),
+                (511, 2, 1024, 5),
+                (511, 0, 1024, 1)])
+def test_ECP(params):
+    lmax, mmax, nlat, nlon = params
+    job = pysharp.sharpjob_d()
+    nalm = ((mmax+1)*(mmax+2))//2 + (mmax+1)*(lmax-mmax)
+    nalm_r = nalm*2-lmax-1
+    alm_r = np.random.uniform(-1., 1., nalm_r)
+    alm = np.empty(nalm, dtype=np.complex128)
+    alm[0:lmax+1] = alm_r[0:lmax+1]
+    alm[lmax+1:] = np.sqrt(0.5)*(alm_r[lmax+1::2] + 1j*alm_r[lmax+2::2])
+
+    job.set_triangular_alm_info(lmax, mmax)
+    job.set_ECP_geometry(nlat, nlon)
+    alm2 = job.map2alm(job.alm2map(alm))
+    assert_allclose(alm, alm2)
