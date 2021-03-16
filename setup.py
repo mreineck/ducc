6733eac7ae30e8c6b8d30bb33db790dc4511a7f2
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-03-16 14:25:46 +0100

    add 'none-debug' compilation strategy

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ user_lflags = os.getenv("DUCC0_LFLAGS", "").split(" ")
 user_lflags = [x for x in user_lflags if x != ""]
 
 compilation_strategy = os.getenv('DUCC0_OPTIMIZATION', 'native')
-if compilation_strategy not in ['none', 'portable', 'portable-debug', 'native', 'native-debug']:
+if compilation_strategy not in ['none', 'none-debug', 'portable', 'portable-debug', 'native', 'native-debug']:
     raise RuntimeError('unknown compilation strategy')
-do_debug = compilation_strategy in ['portable-debug', 'native-debug']
-do_optimize = compilation_strategy not in ['none']
+do_debug = compilation_strategy in ['none-debug', 'portable-debug', 'native-debug']
+do_optimize = compilation_strategy not in ['none', 'none-debug']
 do_native = compilation_strategy in ['native', 'native-debug']
 
 def _get_files_by_suffix(directory, suffix):
