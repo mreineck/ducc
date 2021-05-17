dc7318dd8d7a21d78e2e3d865604f7bf16d488ca
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-05-17 15:32:12 +0200

    more documentation

--- src/ducc0/math/quaternion.h
+++ src/ducc0/math/quaternion.h
@@ -42,9 +42,13 @@ template<typename T> class quaternion_t
   public:
     T x, y, z, w;
 
+    /// Creates an uninitialized quaternion.
     quaternion_t() {}
+    /// Creates a quaternion with the components X, Y, Z, and W.
     quaternion_t(T X, T Y, T Z, T W)
       : x(X), y(Y), z(Z), w(W) {}
+    /// Creates a quaternion which is equivalent to a rotation characterized by
+    /// \a axis and \a angle.
     quaternion_t(const vec3_t<T> &axis, T angle)
       {
       angle*=T(0.5);
@@ -55,30 +59,37 @@ template<typename T> class quaternion_t
       w = cos(angle);
       }
 
+    /// Returns the norm of \a *this.
     T norm() const
       { return x*x+y*y+z*z+w*w; }
 
+    /// Returns \a *this, divided by its norm.
     quaternion_t normalized() const
       {
       T fct = sqrt(T(1)/norm());
       return quaternion_t(x*fct, y*fct, z*fct, w*fct);
       }
 
+    /// Returns \a -(*this)
     quaternion_t operator-() const
       { return quaternion_t(-x,-y,-z,-w); }
 
+    /// Flips the signs of all components.
     void flip()
       { x=-x; y=-y; z=-z; w=-w; }
 
+    /// Returns the conjugate of \a *this.
     quaternion_t conj() const
       { return quaternion_t(-x,-y,-z, w); }
 
+    /// Returns the inverse of \a *this.
     quaternion_t inverse() const
       {
       T fct = T(1)/norm();
       return quaternion_t(-x*fct, -y*fct, -z*fct, w*fct);
       }
 
+    /// Replaces \a *this by \a (*this)*b.
     quaternion_t &operator*= (const quaternion_t &b)
       {
       quaternion_t a=*this;
@@ -89,6 +100,7 @@ template<typename T> class quaternion_t
       return *this;
       }
 
+    /// Returns \a (*this)*b.
     quaternion_t operator* (const quaternion_t &b) const
       {
       return quaternion_t (w*b.x + x*b.w + y*b.z - z*b.y,
@@ -97,9 +109,12 @@ template<typename T> class quaternion_t
                            w*b.w - x*b.x - y*b.y - z*b.z);
       }
 
+    /// Returns the dot product of \a (*this) and \a other.
     T dot(const quaternion_t &other) const
       { return x*other.x + y*other.y + z*other.z + w*other.w; }
 
+    /// Returns a tuple of a rotation axis and an angle which are equivalent
+    /// to \a (*this).
     auto toAxisAngle() const
       {
       T norm = x*x + y*y + z*z;
