2ad36e80d55b9018f658cf8dc650aff514525fa4
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-06-12 17:20:21 +0200

    tweak test script

--- python/demos/sht_analysis_demo.py
+++ python/demos/sht_analysis_demo.py
@@ -25,12 +25,6 @@ def random_alm(lmax, mmax, spin, ncomp):
     return res
 
 
-def synthesize(alm, lmax, spin, nrings, nphi, nthreads, geometry):
-    return ducc0.sht.experimental.synthesis_2d(alm=alm, lmax=lmax, spin=spin, map=np.empty((alm.shape[0], nrings,nphi)), nthreads=nthreads, geometry=geometry)
-
-def analyze(map, lmax, spin, nthreads, geometry):
-    return ducc0.sht.experimental.analysis_2d(alm=np.empty((map.shape[0],nalm(lmax,lmax)),dtype=np.complex128), map=map, lmax=lmax, spin=spin, geometry=geometry, nthreads=nthreads)
-
 def test(lmax, geometry, spin, nthreads=1):
     print("testing lmax={}, spin={}, nthreads={}, geometry={}".format(lmax,spin,nthreads,geometry))
     ncomp = 1 if spin == 0 else 2
@@ -42,16 +36,19 @@ def test(lmax, geometry, spin, nthreads=1):
         nrings = 2*lmax+2
     elif geometry=="F2":
         nrings = 2*lmax+1
+    nphi=2*lmax+2
 
-    alm1 = random_alm(lmax, lmax, spin, ncomp)
+    alm = random_alm(lmax, lmax, spin, ncomp)
+    map = np.empty((alm.shape[0], nrings,nphi))
+    alm2 = np.empty((map.shape[0],nalm(lmax,lmax)),dtype=np.complex128)
     t0=time()
-    blub = synthesize(alm1,lmax,spin,nrings,2*lmax+1, nthreads, geometry)
-    blub2 = analyze(blub,lmax,spin, nthreads, geometry)
+    map = ducc0.sht.experimental.synthesis_2d(alm=alm, lmax=lmax, spin=spin, map=map, nthreads=nthreads, geometry=geometry)
+    alm2 = ducc0.sht.experimental.analysis_2d(alm=alm2, map=map, lmax=lmax, spin=spin, geometry=geometry, nthreads=nthreads)
     print(time()-t0)
-    print("L2 error after full round-trip:", _l2error(blub2,alm1))
+    print("L2 error after full round-trip:", _l2error(alm2,alm))
 
-nthr=16
-for l0 in [4096]:
+nthr=8
+for l0 in [4095]:
     for geometry in ["CC", "F1", "MW", "MWflip", "GL", "DH", "F2"]:
         for spin in [0,1,2]:
             test(lmax=l0, spin=spin, nthreads=nthr, geometry=geometry)
