0f7df667bbf6939fda5a29afa8d0a83517dd0b1e
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-08-31 09:45:57 +0200

    no long doubles on ARM

--- python/test/test_fft.py
+++ python/test/test_fft.py
@@ -105,7 +105,7 @@ ctype = {np.float32: np.complex64,
 
 on_windows = ("microsoft" in platform.uname()[3].lower() or
               platform.system() == "Windows")
-on_arm = ("arm" in platform().system().lower())
+on_arm = ("arm" in platform.machine().lower())
 true_long_double = (np.longfloat != np.float64 and not (on_windows or on_arm))
 dtypes = [np.float32, np.float64]
 if true_long_double:
