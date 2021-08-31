77d4005ac233415f3ec0f2bc35a5b8039285d576
Martin Reinecke <martin@mpa-garching.mpg.de>
2021-08-31 11:14:10 +0200

    test

--- src/ducc0/infra/simd.h
+++ src/ducc0/infra/simd.h
@@ -610,7 +610,7 @@ template<> class helper_<float,4>
       {
       static constexpr int32x4_t shift = {0, 1, 2, 3};
       auto tmp = vshrq_n_u32(v, 31);
-      return vaddvq_u32(vshlq_u32(tmp, shift));
+      return vaddq_u32(vshlq_u32(tmp, shift));
       }
   };
 #endif
