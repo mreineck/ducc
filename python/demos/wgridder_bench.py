ef4043838a99911fe67550367260ea75d5357e2a
Martin Reinecke <martin@mpa-garching.mpg.de>
2022-04-19 20:14:48 +0200

    nicer demo

--- python/demos/wgridder_bench.py
+++ python/demos/wgridder_bench.py
@@ -37,77 +37,134 @@ def main():
 #    ms, fov_deg = '/home/martin/ms/G330.89-0.36.npz', 2.
 #    ms, fov_deg = '/home/martin/ms/bigms.npz', 0.0005556*1800
 #    ms, fov_deg = '/home/martin/ms/L_UV_DATA-IF1.npz', 1.
-    ms, fov_deg = '/data/CYG-ALL-13360-8MHZ.npz', 1.
+#    ms, fov_deg = '/data/CYG-ALL-13360-8MHZ.npz', 0.08
     ms, fov_deg = '/data/L_UV_DATA-IF1.npz', 1.
 
     data = np.load(ms)
     uvw, freq, vis, wgt = data["uvw"], data["freqs"], data["vis"], data["wgt"]
     mask = data["mask"] if "mask" in data else None
-
     wgt[vis == 0] = 0
     if mask is None:
         mask = np.ones(wgt.shape, dtype=np.uint8)
     mask[wgt == 0] = False
     DEG2RAD = np.pi/180
-    nthreads = 8
+    nthreads = 16
     epsilon = 1e-4
     do_wgridding = False
+    verbosity = 1
 
-    ntries_gpu = 1
+    do_sycl = True
+    do_cng = True
+
+    ntries = 2
 
     npixdirty = get_npixdirty(uvw, freq, fov_deg, mask)
     pixsize = fov_deg/npixdirty*DEG2RAD
 
-    print('Start gridding...')
-    t0 = time()
-    dirty = wgridder.vis2dirty(
-        uvw=uvw, freq=freq, vis=vis, wgt=wgt,
-        mask=mask, npix_x=npixdirty, npix_y=npixdirty, pixsize_x=pixsize,
-        pixsize_y=pixsize, epsilon=epsilon, do_wgridding=do_wgridding,
-        nthreads=nthreads, verbosity=1, flip_v=True, gpu=False)
-    t = time() - t0
-    print("{} s".format(t))
-    print("{} visibilities/thread/s".format(np.sum(wgt != 0)/nthreads/t))
-    t0 = time()
-    dirty_g = wgridder.vis2dirty(
-        uvw=uvw, freq=freq, vis=vis, wgt=wgt,
-        mask=mask, npix_x=npixdirty, npix_y=npixdirty, pixsize_x=pixsize,
-        pixsize_y=pixsize, epsilon=epsilon, do_wgridding=do_wgridding,
-        nthreads=nthreads, verbosity=1, flip_v=True, gpu=True)
-    t = time() - t0
-    print("{} s".format(t))
-    print("{} visibilities/thread/s".format(np.sum(wgt != 0)/nthreads/t))
-    print(ducc0.misc.l2error(dirty,dirty_g))
-    fig, axs = plt.subplots(1,3)
-    axs[0].imshow(dirty.T, origin='lower')
-    axs[1].imshow(dirty_g.T, origin='lower')
-    axs[2].imshow(np.minimum(10.,np.maximum(-10.,dirty_g.T/dirty.T)), origin='lower')
-    plt.show()
-    t0 = time()
-    x0 = wgridder.dirty2vis(
-        uvw=uvw, freq=freq, dirty=dirty, wgt=wgt,
-        mask=mask, pixsize_x=pixsize, pixsize_y=pixsize, epsilon=epsilon,
-        do_wgridding=do_wgridding, nthreads=nthreads, verbosity=1,
-        flip_v=True)
-    t = time() - t0
-    print("{} s".format(t))
-    print("{} visibilities/thread/s".format(np.sum(wgt != 0)/nthreads/t))
+  #  vis = vis.astype(np.complex128)
+  #  wgt = wgt.astype(np.float64)
 
-    t0 = time()
-    for _ in range(ntries_gpu):
-        x1 = wgridder.dirty2vis(
+    print('CPU gridding...')
+    print()
+    mintime=1e300
+    for _ in range(ntries):
+        t0 = time()
+        dirty = wgridder.vis2dirty(
+            uvw=uvw, freq=freq, vis=vis, wgt=wgt,
+            mask=mask, npix_x=npixdirty, npix_y=npixdirty, pixsize_x=pixsize,
+            pixsize_y=pixsize, epsilon=epsilon, do_wgridding=do_wgridding,
+            nthreads=nthreads, verbosity=verbosity, flip_v=False, gpu=False,
+            double_precision_accumulation=False)
+        mintime = min(mintime, time()-t0)
+    print()
+    print("Best time: {:.4f} s".format(mintime))
+    print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+    print()
+    if do_sycl:
+        print('SYCL gridding...')
+        mintime=1e300
+        for _ in range(ntries):
+            t0 = time()
+            dirty_g = wgridder.vis2dirty(
+                uvw=uvw, freq=freq, vis=vis, wgt=wgt,
+                mask=mask, npix_x=npixdirty, npix_y=npixdirty, pixsize_x=pixsize,
+                pixsize_y=pixsize, epsilon=epsilon, do_wgridding=do_wgridding,
+                nthreads=nthreads, verbosity=verbosity, flip_v=False, gpu=True,
+                double_precision_accumulation=False)
+            mintime = min(mintime, time()-t0)
+        print("Best time: {:.4f} s".format(mintime))
+        print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+        print("L2 error compared to CPU: {:.2e}".format(ducc0.misc.l2error(dirty,dirty_g)))
+        print()
+    if do_cng:
+        import cuda_nifty_gridder as cng
+        print('ska-gridder-nifty-cuda gridding...')
+        print()
+        mintime=1e300
+        for _ in range(ntries):
+            t0 = time()
+            dirty_cng = cng.ms2dirty(uvw, freq, vis, wgt, npixdirty, npixdirty,
+              pixsize, pixsize, 0, 0, epsilon, do_wgridding, verbosity=verbosity)
+            mintime = min(mintime, time()-t0)
+        print()
+        print("Best time: {:.4f} s".format(mintime))
+        print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+        print("L2 error compared to CPU: {:.2e}".format(ducc0.misc.l2error(dirty,dirty_cng)))
+        print()
+
+    vis_out = vis.copy()
+    print('CPU degridding...')
+    print()
+    mintime=1e300
+    for _ in range(ntries):
+        t0 = time()
+        vis_out = wgridder.dirty2vis(
             uvw=uvw, freq=freq, dirty=dirty, wgt=wgt,
             mask=mask, pixsize_x=pixsize, pixsize_y=pixsize, epsilon=epsilon,
-            do_wgridding=do_wgridding,
-            nthreads=nthreads, verbosity=1,
-            flip_v=True,
-            gpu=True)
-    t = (time() - t0)/ntries_gpu
-    print("{} s".format(t))
-    print("{} visibilities/thread/s".format(np.sum(wgt != 0)/nthreads/t))
-    print(ducc0.misc.l2error(x0,x1))
-    plt.imshow(dirty.T, origin='lower')
-    plt.show()
+            do_wgridding=do_wgridding, nthreads=nthreads, verbosity=verbosity,
+            flip_v=False, vis=vis_out)
+        mintime = min(mintime, time()-t0)
+    print()
+    print("Best time: {:.4f} s".format(mintime))
+    print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+    print()
+    if do_sycl:
+        vis_out_g = vis.copy()
+        print('SYCL degridding...')
+        print()
+        mintime=1e300
+        for _ in range(ntries):
+            t0 = time()
+            vis_out_g = wgridder.dirty2vis(
+                uvw=uvw, freq=freq, dirty=dirty, wgt=wgt,
+                mask=mask, pixsize_x=pixsize, pixsize_y=pixsize, epsilon=epsilon,
+                do_wgridding=do_wgridding,
+                nthreads=nthreads, verbosity=verbosity,
+                flip_v=False,
+                gpu=True, vis=vis_out_g)
+            mintime = min(mintime, time()-t0)
+        print()
+        print("Best time: {:.4f} s".format(mintime))
+        print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+        print("L2 error compared to CPU: {:.2e}".format(ducc0.misc.l2error(vis_out,vis_out_g)))
+        print()
+        del vis_out_g
+    if do_cng:
+        import cuda_nifty_gridder as cng
+        print('ska-gridder-nifty-cuda degridding...')
+        print()
+        mintime=1e300
+        for _ in range(ntries):
+            t0 = time()
+            vis_out_cng = cng.dirty2ms(uvw, freq, dirty, wgt, pixsize, pixsize, 0, 0, epsilon, do_wgridding, verbosity=verbosity)
+            mintime = min(mintime, time()-t0)
+            vis_out_cng *= wgt
+        print()
+        print("Best time: {:.4f} s".format(mintime))
+        print("{:.2f} Mvis/s".format(np.sum(wgt != 0)/mintime/1e6))
+        print("L2 error compared to CPU: {:.2e}".format(ducc0.misc.l2error(vis_out,vis_out_cng)))
+        print()
+        del vis_out_cng
 
 
 if __name__ == "__main__":
