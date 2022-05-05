ef49bd68a845f66348e4b859524918a902f7b329
Martin Reinecke <martin@mpa-garching.mpg.de>
2022-05-05 12:26:30 +0200

    small simplifications

--- python/demos/totalconvolve_any_hwp.py
+++ python/demos/totalconvolve_any_hwp.py
@@ -82,7 +82,9 @@ class MuellerConvolver:
         # n*pi/5 for n in [0; 5[
         sqrt2 = np.sqrt(2.)
         nbeam = 5
-        blm_eff = [[self.AlmPM(lmax, mmax+4) for _ in range(4)] for _ in range(nbeam)]
+        inc = 4
+        res = np.zeros((nbeam, ncomp, nalm(lmax, mmax+inc)), dtype=np.complex128)
+        blm_eff = [self.AlmPM(lmax, mmax+4) for _ in range(4)]
         for ibeam in range(nbeam):
             alpha = ibeam*np.pi/nbeam
             e2ia = np.exp(2*1j*alpha)
@@ -92,49 +94,45 @@ class MuellerConvolver:
             for m in range(-mmax-4, mmax+4+1):
                 for l in range(abs(m), lmax+1):
                     # T component, Marta notes [4a]
-                    blm_eff[ibeam][0][l, m] = \
+                    blm_eff[0][l, m] = \
                           C[0,0]*blm2[0][l,m] \
                         + C[3,0]*blm2[3][l,m] \
                         + 1./sqrt2*(C[1,0]*blm2[2][l,m+2]*e2ia \
                                   + C[2,0]*blm2[1][l,m-2]*e2iac)
                     # V component, Marta notes [4d]
-                    blm_eff[ibeam][3][l, m] = \
+                    blm_eff[3][l, m] = \
                           C[0,3]*blm2[0][l,m] \
                         + C[3,3]*blm2[3][l,m] \
                         + 1./sqrt2*(C[1,3]*blm2[2][l,m+2]*e2ia \
                                   + C[2,3]*blm2[1][l,m-2]*e2iac)
                     # E/B components, Marta notes [4b,c]
-                    blm_eff[ibeam][1][l, m] = \
+                    blm_eff[1][l, m] = \
                           sqrt2*e2iac*(C[0,1]*blm2[0][l,m+2] \
                                    + C[3,1]*blm2[3][l,m+2]) \
                         + C[2,1]*e4iac*blm2[2][l,m+4] \
                         + C[1,1]*blm2[1][l,m]
-                    blm_eff[ibeam][2][l, m] = \
+                    blm_eff[2][l, m] = \
                           sqrt2*e2ia*(C[0,2]*blm2[0][l,m-2] \
                                     + C[3,2]*blm2[3][l,m-2]) \
                         + C[1,2]*e4ia*blm2[1][l,m-4] \
                         + C[2,2]*blm2[2][l,m]
 
-        # back to original TEBV b_lm format
-        inc = 4
-        res = np.zeros((nbeam, ncomp, nalm(lmax, mmax+inc)), dtype=np.complex128)
-
-        for ibeam in range(nbeam):
+            # back to original TEBV b_lm format
             idx = 0
             for m in range(mmax+inc+1):
                 for l in range(m, lmax+1):
                     # T component
-                    res[ibeam, 0, idx] = blm_eff[ibeam][0][l, m]
+                    res[ibeam, 0, idx] = blm_eff[0][l, m]
                     # V component
                     if ncomp > 3:
-                        res[ibeam, 3, idx] = blm_eff[ibeam][3][l, m]
+                        res[ibeam, 3, idx] = blm_eff[3][l, m]
                     # E/B components
                     if ncomp > 2:
                         # Adri's notes [10]
-                        res[ibeam, 1, idx] = -0.5*(blm_eff[ibeam][1][l, m] \
-                                                  +blm_eff[ibeam][2][l, m])
-                        res[ibeam, 2, idx] = 0.5j*(blm_eff[ibeam][1][l, m] \
-                                                  -blm_eff[ibeam][2][l, m])
+                        res[ibeam, 1, idx] = -0.5*(blm_eff[1][l, m] \
+                                                  +blm_eff[2][l, m])
+                        res[ibeam, 2, idx] = 0.5j*(blm_eff[1][l, m] \
+                                                  -blm_eff[2][l, m])
                     idx += 1
         return res
 
@@ -160,11 +158,14 @@ class MuellerConvolver:
         self._kmax = kmax
         tmp = self.mueller_tc_prep (blm, mueller, lmax, kmax)
         tmp = self.pseudo_fft(tmp)
-        maxval = [np.max(np.abs(x)) for x in tmp]
-        maxmax = max(maxval)
+
         # construct the five interpolators for the individual components
         # With some enhancements in the C++ code I could put this into a single
-        # interpolator fo better performance,but for now this should do.
+        # interpolator for better performance, but for now this should do.
+        # If the blm for any interpolator are very small in comparison to the
+        # others, the interpolator is replaced by a `None` object.
+        maxval = [np.max(np.abs(x)) for x in tmp]
+        maxmax = max(maxval)
         self._inter = []
         for i in range(5):
             if maxval[i] > 1e-10*maxmax:  # component is not zero
