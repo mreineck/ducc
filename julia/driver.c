3d119cc979098d2b7eefedb642424d69941cf652
Martin Reinecke <martin@mpa-garching.mpg.de>
2022-09-28 15:04:39 +0200

    beginnings of a Julia interface

---
+++ julia/driver.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+void nufft_u2nu_julia_double (size_t ndim,
+                              size_t npoints,
+                              const size_t *shape,
+                              const double *grid,
+                              const double *coord,
+                              int forward,
+                              double epsilon,
+                              size_t nthreads,
+                              double *out,
+                              size_t verbosity,
+                              double sigma_min,
+                              double sigma_max);
+
+void nufft_nu2u_julia_double (size_t ndim,
+                              size_t npoints,
+                              const size_t *shape,
+                              const double *points,
+                              const double *coord,
+                              int forward,
+                              double epsilon,
+                              size_t nthreads,
+                              double *out,
+                              size_t verbosity,
+                              double sigma_min,
+                              double sigma_max);
+int main()
+  {
+  int ndim=3;
+  int npoints=10000000;
+  size_t shape[]={100,200,300};
+  double *grid = calloc(shape[0]*shape[1]*shape[2], 2*sizeof(double));
+  double *coord = calloc(ndim*npoints, sizeof(double));
+  double *points = calloc(npoints, 2*sizeof(double));
+  nufft_u2nu_julia_double (ndim, npoints, shape, grid, coord, 1, 1e-5, 4, points, 1, 1.1, 2.5);
+  nufft_nu2u_julia_double (ndim, npoints, shape, points, coord, 1, 1e-5, 4, grid, 1, 1.1, 2.5);
+  }
---
+++ julia/ducc_julia.cc
@@ -0,0 +1,91 @@
+// file ducc_julia.cc
+
+/*
+Compilation: (the -I path must point to the src/ directory in the ducc0 checkout)
+
+g++ -O3 -march=native -ffast-math -I ../src/ ducc_julia.cc -Wfatal-errors -pthread -std=c++17 -fPIC -c
+
+Creating the shared library:
+
+g++ -O3 -march=native -o ducc_julia.so ducc_julia.o -Wfatal-errors -pthread -std=c++17 -shared -fPIC
+*/
+
+#include "ducc0/infra/threading.cc"
+#include "ducc0/nufft/nufft.h"
+
+using namespace ducc0;
+using namespace std;
+
+extern "C" {
+
+/*
+ndim:    number of dimensions (1/2/3)
+npoints: number of non-uniform points
+shape:   points to a dense Julia array of shape(ndim,) containing the grid
+         dimensions in Julia order
+grid:    points to a dense Julia array of shape (2,shape)
+         the leading dimension is for real and imaginary parts
+coord:   points to a dense Julia array of shape(ndim,npoints)
+forward: 0 ==> FFT exponent is  1
+         1 ==> FFT exponent is -1
+out:     points to a mutable dense Julia array of shape (2,npoints)
+         the leading dimension is for real and imaginary parts
+*/
+void nufft_u2nu_julia_double (size_t ndim,
+                              size_t npoints,
+                              const size_t *shape,
+                              const double *grid,
+                              const double *coord,
+                              int forward,
+                              double epsilon,
+                              size_t nthreads,
+                              double *out,
+                              size_t verbosity,
+                              double sigma_min,
+                              double sigma_max)
+  {
+  cmav<double,2> mycoord(coord,{npoints,ndim});
+  vector<size_t> myshape(ndim);
+  for (size_t i=0; i<ndim; ++i)
+    myshape[i] = shape[ndim-1-i];
+  cfmav<complex<double>> mygrid(reinterpret_cast<const complex<double> *>(grid), myshape);
+  vmav<complex<double>,1> myout(reinterpret_cast<complex<double> *>(out), {npoints});
+  u2nu<double,double>(mycoord,mygrid,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max);
+  }
+
+/*
+ndim:    number of dimensions (1/2/3)
+npoints: number of non-uniform points
+shape:   points to a dense Julia array of shape(ndim,) containing the grid
+         dimensions in Julia order
+points:  points to a dense Julia array of shape (2,npoints)
+         the leading dimension is for real and imaginary parts
+coord:   points to a dense Julia array of shape(ndim,npoints)
+forward: 0 ==> FFT exponent is  1
+         1 ==> FFT exponent is -1
+out:     points to a dense mutable Julia array of shape (2,shape)
+         the leading dimension is for real and imaginary parts
+*/
+void nufft_nu2u_julia_double (size_t ndim,
+                              size_t npoints,
+                              const size_t *shape,
+                              const double *points,
+                              const double *coord,
+                              int forward,
+                              double epsilon,
+                              size_t nthreads,
+                              double *out,
+                              size_t verbosity,
+                              double sigma_min,
+                              double sigma_max)
+  {
+  cmav<double,2> mycoord(coord,{npoints,ndim});
+  vector<size_t> myshape(ndim);
+  for (size_t i=0; i<ndim; ++i)
+    myshape[i] = shape[ndim-1-i];
+  vfmav<complex<double>> myout(reinterpret_cast<complex<double> *>(out), myshape);
+  cmav<complex<double>,1> mypoints(reinterpret_cast<const complex<double> *>(points), {npoints});
+  nu2u<double,double>(mycoord,mypoints,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max);
+  }
+
+}
