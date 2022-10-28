991a5a12390a87842f68b9dcaf8324f35aba5066
Martin Reinecke <martin@mpa-garching.mpg.de>
2022-10-28 15:06:09 +0200

    sync with changed nufft interface

--- julia/driver.c
+++ julia/driver.c
@@ -11,7 +11,9 @@ void nufft_u2nu_julia_double (size_t ndim,
                               double *out,
                               size_t verbosity,
                               double sigma_min,
-                              double sigma_max);
+                              double sigma_max,
+                              double periodicity,
+                              int fft_order);
 
 void nufft_nu2u_julia_double (size_t ndim,
                               size_t npoints,
@@ -24,15 +26,18 @@ void nufft_nu2u_julia_double (size_t ndim,
                               double *out,
                               size_t verbosity,
                               double sigma_min,
-                              double sigma_max);
+                              double sigma_max,
+                              double periodicity,
+                              int fft_order);
 int main()
   {
+  const double pi=3.141592653589793238462643383279502884197;
   int ndim=3;
   int npoints=10000000;
   size_t shape[]={100,200,300};
   double *grid = calloc(shape[0]*shape[1]*shape[2], 2*sizeof(double));
   double *coord = calloc(ndim*npoints, sizeof(double));
   double *points = calloc(npoints, 2*sizeof(double));
-  nufft_u2nu_julia_double (ndim, npoints, shape, grid, coord, 1, 1e-5, 4, points, 1, 1.1, 2.5);
-  nufft_nu2u_julia_double (ndim, npoints, shape, points, coord, 1, 1e-5, 4, grid, 1, 1.1, 2.5);
+  nufft_u2nu_julia_double (ndim, npoints, shape, grid, coord, 1, 1e-5, 4, points, 1, 1.1, 2.5, 2*pi, 0);
+  nufft_nu2u_julia_double (ndim, npoints, shape, points, coord, 1, 1e-5, 4, grid, 1, 1.1, 2.5, 2*pi, 0);
   }
--- julia/ducc_julia.cc
+++ julia/ducc_julia.cc
@@ -11,6 +11,9 @@ g++ -O3 -march=native -o ducc_julia.so ducc_julia.o -Wfatal-errors -pthread -std
 */
 
 #include "ducc0/infra/threading.cc"
+#include "ducc0/infra/mav.cc"
+#include "ducc0/math/gl_integrator.cc"
+#include "ducc0/math/gridding_kernel.cc"
 #include "ducc0/nufft/nufft.h"
 
 using namespace ducc0;
@@ -19,17 +22,24 @@ using namespace std;
 extern "C" {
 
 /*
-ndim:    number of dimensions (1/2/3)
-npoints: number of non-uniform points
-shape:   points to a dense Julia array of shape(ndim,) containing the grid
-         dimensions in Julia order
-grid:    points to a dense Julia array of shape (2,shape)
-         the leading dimension is for real and imaginary parts
-coord:   points to a dense Julia array of shape(ndim,npoints)
-forward: 0 ==> FFT exponent is  1
-         1 ==> FFT exponent is -1
-out:     points to a mutable dense Julia array of shape (2,npoints)
-         the leading dimension is for real and imaginary parts
+ndim:         number of dimensions (1/2/3)
+npoints:      number of non-uniform points
+shape:        points to a dense Julia array of shape(ndim,) containing the grid
+              dimensions in Julia order
+grid:         points to a dense Julia array of shape (2,shape)
+              the leading dimension is for real and imaginary parts
+coord:        points to a dense Julia array of shape(ndim,npoints)
+forward:      0 ==> FFT exponent is  1
+              1 ==> FFT exponent is -1
+epsilon:      desired accuracy
+nthreads:     number of threads to use. 0 means using all available threads. 
+out:          points to a mutable dense Julia array of shape (2,npoints)
+              the leading dimension is for real and imaginary parts
+sigma_min:    minimum oversampling factor. If unsure, use 1.1
+sigma_max:    maximum oversampling factor. If unsure, use 2.6
+periodicity:  assumed periodicity of the coordinates of the nonuniform points
+fft_order:    0: Fourier grids start with most negative k mode
+              1: Fourier grids start with zero k mode (usual FFT convention)
 */
 void nufft_u2nu_julia_double (size_t ndim,
                               size_t npoints,
@@ -42,7 +52,9 @@ void nufft_u2nu_julia_double (size_t ndim,
                               double *out,
                               size_t verbosity,
                               double sigma_min,
-                              double sigma_max)
+                              double sigma_max,
+                              double periodicity,
+                              int fft_order)
   {
   cmav<double,2> mycoord(coord,{npoints,ndim});
   vector<size_t> myshape(ndim);
@@ -50,21 +62,28 @@ void nufft_u2nu_julia_double (size_t ndim,
     myshape[i] = shape[ndim-1-i];
   cfmav<complex<double>> mygrid(reinterpret_cast<const complex<double> *>(grid), myshape);
   vmav<complex<double>,1> myout(reinterpret_cast<complex<double> *>(out), {npoints});
-  u2nu<double,double>(mycoord,mygrid,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max);
+  u2nu<double,double>(mycoord,mygrid,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max,periodicity,fft_order);
   }
 
 /*
-ndim:    number of dimensions (1/2/3)
-npoints: number of non-uniform points
-shape:   points to a dense Julia array of shape(ndim,) containing the grid
-         dimensions in Julia order
-points:  points to a dense Julia array of shape (2,npoints)
-         the leading dimension is for real and imaginary parts
-coord:   points to a dense Julia array of shape(ndim,npoints)
-forward: 0 ==> FFT exponent is  1
-         1 ==> FFT exponent is -1
-out:     points to a dense mutable Julia array of shape (2,shape)
-         the leading dimension is for real and imaginary parts
+ndim:         number of dimensions (1/2/3)
+npoints:      number of non-uniform points
+shape:        points to a dense Julia array of shape(ndim,) containing the grid
+              dimensions in Julia order
+points:       points to a dense Julia array of shape (2,npoints)
+              the leading dimension is for real and imaginary parts
+coord:        points to a dense Julia array of shape(ndim,npoints)
+forward:      0 ==> FFT exponent is  1
+              1 ==> FFT exponent is -1
+epsilon:      desired accuracy
+nthreads:     number of threads to use. 0 means using all available threads. 
+out:          points to a dense mutable Julia array of shape (2,shape)
+              the leading dimension is for real and imaginary parts
+sigma_min:    minimum oversampling factor. If unsure, use 1.1
+sigma_max:    maximum oversampling factor. If unsure, use 2.6
+periodicity:  assumed periodicity of the coordinates of the nonuniform points
+fft_order:    0: Fourier grids start with most negative k mode
+              1: Fourier grids start with zero k mode (usual FFT convention)
 */
 void nufft_nu2u_julia_double (size_t ndim,
                               size_t npoints,
@@ -77,7 +96,9 @@ void nufft_nu2u_julia_double (size_t ndim,
                               double *out,
                               size_t verbosity,
                               double sigma_min,
-                              double sigma_max)
+                              double sigma_max,
+                              double periodicity,
+                              int fft_order)
   {
   cmav<double,2> mycoord(coord,{npoints,ndim});
   vector<size_t> myshape(ndim);
@@ -85,7 +106,7 @@ void nufft_nu2u_julia_double (size_t ndim,
     myshape[i] = shape[ndim-1-i];
   vfmav<complex<double>> myout(reinterpret_cast<complex<double> *>(out), myshape);
   cmav<complex<double>,1> mypoints(reinterpret_cast<const complex<double> *>(points), {npoints});
-  nu2u<double,double>(mycoord,mypoints,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max);
+  nu2u<double,double>(mycoord,mypoints,forward,epsilon,nthreads,myout,verbosity,sigma_min,sigma_max,periodicity,fft_order);
   }
 
 }
