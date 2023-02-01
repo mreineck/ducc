56154b70f42dd0e8229fded67a3bb35a34b270d2
Martin Reinecke <martin@mpa-garching.mpg.de>
2023-02-01 17:47:57 +0100

    update copyright

--- src/ducc0/wgridder/wgridder.h
+++ src/ducc0/wgridder/wgridder.h
@@ -14,7 +14,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/* Copyright (C) 2019-2022 Max-Planck-Society
+/* Copyright (C) 2019-2023 Max-Planck-Society
    Author: Martin Reinecke */
 
 #ifndef DUCC0_WGRIDDER_H
