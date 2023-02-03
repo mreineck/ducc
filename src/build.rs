e4120676413f6c51c70686e0feb44c869a61bda7
Philipp Arras <c@philipp-arras.de>
2023-02-03 13:11:58 +0100

    Generate random ndarray

--- src/build.rs
+++ src/build.rs
@@ -3,6 +3,7 @@ use cxx_build::CFG;
 fn main() {
     CFG.include_prefix = "";
     cxx_build::bridge("src/main.rs")
-    //.file("ducc.cc")
+    //.file("test.cc")
+    .flag_if_supported("-std=c++17")
     .compile("ducc-demo");
 }
--- src/src/main.rs
+++ src/src/main.rs
@@ -1,24 +1,35 @@
-// use ndarray::Array;
-// use ndarray_rand::RandomExt;
-// use ndarray_rand::rand_distr::Uniform;
-// 
+use ndarray::{Array, s};
+use ndarray_rand::RandomExt;
+use ndarray_rand::rand_distr::Uniform;
+
 // use num_complex::Complex;
 
 
+// static constexpr size_t maxdim=10;
+//
+// array<uint64_t, maxdim> shape;
+// array<int64_t, maxdim> stride;
+//
+// void *data;
+// uint8_t ndim;
+// uint8_t dtype;
 
 #[cxx::bridge(namespace = "ducc0")]
 mod ffi {
     unsafe extern "C++" {
         include!("ducc0/bindings/array_descriptor.h");
+
+        // type ArrayDescriptor;
+
+        // fn printArrayDescriptor(inp: ArrayDescriptor) -> ;
     }
 }
 
 fn main() {
-    //CFG.exported_header_dirs.push("ducc0/ducc/src");
-    //CFG.exported_header_dirs.push("./ducc/src");
-    //CFG.exported_header_dirs.push("./ducc/src/ducc0");
-    //CFG.exported_header_dirs.push("ducc0/ducc/src/ducc0");
-
-    // let a = Array::random((2, 5), Uniform::new(0., 10.));
-    // println!("{:8.4}", a);
+    let a = Array::random((5, 6, 7), Uniform::new(0., 10.));
+    let slice = a.slice(s![.., 1, ..]);
+    println!("Whole array");
+    println!("{:8.4}", a);
+    println!("Slice");
+    println!("{:8.4}", slice);
 }
