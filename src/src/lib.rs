34408a5e81ff970bfb09be67353ea43e8341ac3a
Philipp Arras <c@philipp-arras.de>
2023-02-23 21:27:54 +0100

    Cleanup

--- src/src/lib.rs
+++ src/src/lib.rs
@@ -1,10 +1,8 @@
-use ndarray::{ArrayView, Dimension, ArrayViewMut, array, ArrayBase, Array1};
+use ndarray::{ArrayView, Dimension, ArrayViewMut, Array1};
 use num_complex::Complex;
 use std::any::TypeId;
 use std::ffi::c_void;
 use std::mem::size_of;
-
-// TODO Error handling
 
 #[repr(C)]
 pub struct RustArrayDescriptor {
@@ -32,7 +30,6 @@ fn format_stride(ndinp: &[isize]) -> [i64; 10] {
 }
 
 extern "C" {
-    fn c_square(inp: &mut RustArrayDescriptor);
     fn c2c_double(
         inp: &RustArrayDescriptor,
         out: &mut RustArrayDescriptor,
@@ -41,23 +38,8 @@ extern "C" {
         fct: f64,
         nthreads: usize,
     );
-    fn c2c_inplace_double(
-        inout: &mut RustArrayDescriptor,
-        axes: &RustArrayDescriptor,
-        forward: bool,
-        fct: f64,
-        nthreads: usize,
-    );
 }
 
-pub fn square<A: 'static, D: ndarray::Dimension>(inp: ArrayViewMut<A, D>) {
-    let mut inp2 = mutslice2arrdesc(inp);
-    unsafe {
-        c_square(&mut inp2);
-    }
-}
-
-// TODO proper handling of fct dtype
 pub fn c2c<A: 'static, D: ndarray::Dimension>(inp: ArrayView<Complex<A>, D>, out: ArrayViewMut<Complex<A>, D>, axes: Vec<usize>,
     forward: bool, fct: f64, nthreads: usize ) {
     let inp2 = slice2arrdesc(inp);
@@ -122,51 +104,15 @@ fn slice2arrdesc<'a, A: 'static, D: Dimension>(slc: ArrayView<'a, A, D>) -> Rust
     }
 }
 
-// fn main() {
-//     let a = Array::random((5, 6, 7), Uniform::new(0. as f32, 11. as f32));
-//     let slice = a.slice(s![.., 1, ..]);
-//     let mut arr = slice2arrdesc(slice);
-//
-//     println!("{:8.4}", slice);
-//     unsafe {
-//         square(&mut arr);
-//     }
-//     println!("{:8.4}", slice);
-//
-//     let m_re = Array::<f64, _>::random((5, 6), Uniform::new(-1.0, 1.0));
-//     let m_im = Array::<f64, _>::random((5, 6), Uniform::new(-1.0, 1.0));
-//     let mut m_c = Array::<Complex<f64>, _>::zeros((5, 6));
-//     Zip::from(&mut m_c)
-//         .and(&m_re)
-//         .and(&m_im)
-//         .for_each(|c, &re, &im| {
-//             *c = Complex { re, im };
-//         });
-//
-//     let mut arr2 = slice2arrdesc(m_c.slice(s![.., ..])); // TODO Simplify!
-//     println!("{:8.4}", m_c);
-//     unsafe {
-//         square(&mut arr2);
-//     }
-//     println!("{:8.4}", m_c);
-// }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use ndarray::{s, Array, IxDyn, Zip, Dim};
-    use ndarray_rand::rand_distr::Uniform;
-    use ndarray_rand::RandomExt;
+    use ndarray::{Array, Dim};
 
     #[test]
     fn square_test() {
         let shape = (2, 3, 3);
-        let mut a = Array::random(shape, Uniform::new(-1., 10.));
-
-        // println!("{:8.4}", a);
-        // square(a.view_mut());
-        // println!("{:8.4}", a);
-
         let b = Array::ones(shape);
         let mut c = Array::ones(shape);
         let axes = vec![0, 2];
