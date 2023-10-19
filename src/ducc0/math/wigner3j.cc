e302a238b6e232c2ca520b8d4c17e3a1d45ff262
Martin Reinecke <martin@mpa-garching.mpg.de>
2023-10-19 13:37:39 +0200

    cleanup round 1

--- src/ducc0/math/wigner3j.cc
+++ src/ducc0/math/wigner3j.cc
@@ -134,52 +134,58 @@ void wigner3j_00_internal (double l2, double l3, double l1min, double l1max,
     res(k)*=cnorm;
   }
 
-// version for m2==-m3
-void wigner3j_symm_m2m3_internal (double l2, double l3, double m2,
-                        double l1min, double l1max, int ncoef,
-                        vmav<double,1> &res)
+
+template<size_t bufsize> void wigner3j_internal_block
+  (double l2, double l3, double m2, double m3,
+   double m1, double l1min, double l1max, int ncoef,
+   vmav<double,1> &res)
   {
   constexpr double srhuge=0x1p+250,
                    tiny=0x1p-500, srtiny=0x1p-250;
 
   const double l2ml3sq = (l2-l3)*(l2-l3),
                pre1 = (l2+l3+1.)*(l2+l3+1.),
-               m3mm2 = -2*m2;
+               m1sq = m1*m1,
+               pre2 = m1*(l2*(l2+1.)-l3*(l3+1.)),
+               m3mm2 = m3-m2;
 
-  int i=0;
+  using Tv = native_simd<double>;
+  constexpr size_t vlen=Tv::size();
+  constexpr size_t nvec=bufsize/vlen;
+  static_assert(nvec*vlen==bufsize, "illegal bufsize");
+  union Tbuf
+    {
+    array<double, bufsize> s;
+    array<Tv, nvec> v;
+    };
+  Tbuf l1ladder, c1v, c2v, newfacv;
+
   double c1=0x1p+1000;
   double oldfac=0.;
+  for (size_t m=0; m<bufsize; ++m)
+    l1ladder.s[m] = m;
 
   MR_assert(res.shape(0)==size_t(ncoef), "bad size of result array");
 
+  int i=0;
   res(i) = srtiny;
   double sumfor = (2.*l1min+1.) * res(i)*res(i);
 
-  while(true)
+  // do first iteration separately
+  if (i+1<ncoef)
     {
-    if (i+1==ncoef) break; /* all done */
     ++i;
 
-    const double l1 = l1min+i,
-                 l1sq = l1*l1,
+    const double l1 = l1min+i;
+    const double l1sq = l1*l1,
                  c1old = abs(c1),
-                 newfac = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*l1sq);
-
-    if (i>1)
-      {
-      const double tmp1 = 1./((l1-1.)*newfac);
-      c1 = (2.*l1-1.)*(-(l1sq-l1)*m3mm2) * tmp1;
-      res(i) = res(i-1)*c1 - res(i-2)*l1*oldfac*tmp1;
-      }
-    else
-      {
-      c1 = (l1>1.000001) ? (2.*l1-1.)*(-(l1sq-l1)*m3mm2)/((l1-1.)*newfac)
-                         : -(2.*l1-1.)*l1*m3mm2/newfac;
-      res(i) = res(i-1)*c1;
-      }
-
+                 newfac = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
+    c1 = (l1>1.000001) ? (2.*l1-1.)*(pre2-(l1sq-l1)*m3mm2)/((l1-1.)*newfac)
+                       : -(2.*l1-1.)*l1*(m3mm2)/newfac;
+    res(i) = res(i-1)*c1;
+  
     oldfac=newfac;
-
+  
     sumfor += (2.*l1+1.)*res(i)*res(i);
     if (abs(res(i))>=srhuge)
       {
@@ -187,8 +193,47 @@ void wigner3j_symm_m2m3_internal (double l2, double l3, double m2,
         res(k)*=srtiny;
       sumfor*=tiny;
       }
-    if (c1old<=abs(c1)) break;
     }
+
+  while(true)
+    {
+// prepare buffers
+    for (size_t m=0; m<nvec; ++m)
+      {
+      auto l1v = double(l1min+i+1)+l1ladder.v[m];
+      auto l1sq = l1v*l1v;
+      newfacv.v[m] = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
+      auto tmp1 = native_simd<double>(1.)/((l1v-1.)*newfacv.v[m]);
+      c1v.v[m] = (2.*l1v-1.)*(pre2-(l1sq-l1v)*m3mm2) * tmp1;
+      c2v.v[m] = l1v*tmp1;
+      }
+
+    int ilim = min(ncoef, i+1+int(bufsize));
+    int vidx=0;
+    while(i+1<ilim)
+      {
+      ++i;
+      const double l1 = l1min+i,
+                   c1old = abs(c1);
+      c1 = c1v.s[vidx];
+      res(i) = res(i-1)*c1 - res(i-2)*c2v.s[vidx]*oldfac;
+
+      oldfac=newfacv.s[vidx];
+
+      sumfor += (2.*l1+1.)*res(i)*res(i);
+      if (abs(res(i))>=srhuge)
+        {
+        for (int k=0; k<=i; ++k)
+          res(k)*=srtiny;
+        sumfor*=tiny;
+        }
+      if (c1old<=abs(c1)) goto bailout_fwd;
+      ++vidx;
+      }
+    if (i+1>=ncoef) goto bailout_fwd;
+    }
+
+bailout_fwd:
 
   double sumbac=0.;
   bool last_coeff_is_negative=false;
@@ -204,33 +249,65 @@ void wigner3j_symm_m2m3_internal (double l2, double l3, double m2,
     res(i) = srtiny;
     sumbac = (2.*l1max+1.) * res(i)*res(i);
 
-    do
+    {
+    --i;
+
+    const double l1 = l1min+i,
+                 l1p1sq = (l1+1.)*(l1+1.),
+                 newfac = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
+
+    res(i) = res(i+1)*(2.*l1+3.)*(pre2-(l1p1sq+l1+1.)*m3mm2)
+             /((l1+2.)*newfac);
+
+    oldfac=newfac;
+
+    sumbac += (2.*l1+1.)*res(i)*res(i);
+    if (abs(res(i))>=srhuge)
       {
-      --i;
-
-      const double l1 = l1min+i,
-                   l1p1sq = (l1+1.)*(l1+1.),
-                   newfac = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*l1p1sq);
-
-      if (i<ncoef-2)
-        res(i) = (res(i+1) * (2.*l1+3.)*(-(l1p1sq+l1+1.)*m3mm2)
-                 -res(i+2) * (l1+1.)*oldfac)
-                 / ((l1+2.)*newfac);
-      else
-        res(i) = res(i+1)*(2.*l1+3.)*(-(l1p1sq+l1+1.)*m3mm2)
-                 /((l1+2.)*newfac);
-
-      oldfac=newfac;
-
-      sumbac += (2.*l1+1.)*res(i)*res(i);
-      if (abs(res(i))>=srhuge)
-        {
-        for (int k=i; k<ncoef; ++k)
-          res(k)*=srtiny;
-        sumbac*=tiny;
-        }
+      for (int k=i; k<ncoef; ++k)
+        res(k)*=srtiny;
+      sumbac*=tiny;
       }
-    while (i>nstep2);
+    }
+
+    for (size_t m=0; m<bufsize; ++m)
+      l1ladder.s[m] = -int(m);
+  
+    while(true)
+      {
+      // prepare buffers
+      for (size_t m=0; m<nvec; ++m)
+        {
+        auto l1p1v = double(l1min+i)+l1ladder.v[m];
+        auto l1p1sq = l1p1v*l1p1v;
+        newfacv.v[m] = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
+        auto tmp1 = native_simd<double>(1.)/((l1p1v+1.)*newfacv.v[m]);
+        c1v.v[m] = (2.*l1p1v+1.)*(pre2-(l1p1sq+l1p1v)*m3mm2) * tmp1;
+        c2v.v[m] = l1p1v*tmp1;
+        }
+  
+      int ilim = max(nstep2, i-int(bufsize));
+      int vidx=0;
+      while(i>ilim)
+        {
+        --i;
+        const double l1 = l1min+i;
+        res(i) = res(i+1)*c1v.s[vidx] - res(i+2)*c2v.s[vidx]*oldfac;
+        oldfac=newfacv.s[vidx];
+  
+        sumbac += (2.*l1+1.)*res(i)*res(i);
+        if (abs(res(i))>=srhuge)
+          {
+          for (int k=i; k<ncoef; ++k)
+            res(k)*=srtiny;
+          sumbac*=tiny;
+          }
+        ++vidx;
+        }
+      if (i<=nstep2) goto bailout_bwd;
+      }
+
+bailout_bwd:
 
     for (size_t i=nstep2; i<min(ncoef,nstep2+3); ++i)
       {
@@ -249,25 +326,10 @@ void wigner3j_symm_m2m3_internal (double l2, double l3, double m2,
     {
     last_coeff_is_negative = res(ncoef-1)<0.;
     }
-#if 0
-xxxx
-	if(fabs(ratio)<1) {
-	  nlim++;
-	  ratio=1./ratio;
-	  for(ii=nlim-1;ii<nfin;ii++) //is the index ok??
-	    thrcof[ii]*=ratio;
-	  sumuni=ratio*ratio*sumbac+sumfor;
-	}
-	else {
-	  for(ii=0;ii<nlim;ii++)
-	    thrcof[ii]*=ratio;
-	  sumuni=ratio*ratio*sumfor+sumbac;
-	}
-xxxx
-#endif
+
   double cnorm=1./sqrt(sumfor+sumbac);
   // follow sign convention: sign(f(l_max)) = (-1)**(l2-l3+m2+m3)
-  bool last_coeff_should_be_negative = nearest_int(abs(l2-l3))&1;
+  bool last_coeff_should_be_negative = nearest_int(abs(l2-l3+m2+m3))&1;
   if (last_coeff_is_negative != last_coeff_should_be_negative)
     cnorm = -cnorm;
 
@@ -284,8 +346,9 @@ void wigner3j_internal (double l2, double l3, double m2, double m3,
   {
   if ((m2==0.) && (m3==0.))
     return wigner3j_00_internal (l2, l3, l1min, l1max, ncoef, res);
-  if (m2==-m3)
-    return wigner3j_symm_m2m3_internal (l2, l3, m2, l1min, l1max, ncoef, res);
+
+  if constexpr (native_simd<double>::size()>=4)
+    return wigner3j_internal_block<16> (l2, l3, m2, m3, m1, l1min, l1max, ncoef, res);
 
   constexpr double srhuge=0x1p+250,
                    tiny=0x1p-500, srtiny=0x1p-250;
@@ -412,419 +475,6 @@ void wigner3j_internal (double l2, double l3, double m2, double m3,
     res(k)*=cnorm*fct_bwd;
   }
 
-template<size_t bufsize> void wigner3j_internal_block
-  (double l2, double l3, double m2, double m3,
-   double m1, double l1min, double l1max, int ncoef,
-   vmav<double,1> &res)
-  {
-  if ((m2==0.) && (m3==0.))
-    return wigner3j_00_internal (l2, l3, l1min, l1max, ncoef, res);
-//  if (m2==-m3)
-//    return wigner3j_symm_m2m3_internal (l2, l3, m2, l1min, l1max, ncoef, res);
-
-  constexpr double srhuge=0x1p+250,
-                   tiny=0x1p-500, srtiny=0x1p-250;
-
-  const double l2ml3sq = (l2-l3)*(l2-l3),
-               pre1 = (l2+l3+1.)*(l2+l3+1.),
-               m1sq = m1*m1,
-               pre2 = m1*(l2*(l2+1.)-l3*(l3+1.)),
-               m3mm2 = m3-m2;
-
-  using Tv = native_simd<double>;
-  constexpr size_t vlen=Tv::size();
-  constexpr size_t nvec=bufsize/vlen;
-  static_assert(nvec*vlen==bufsize, "illegal bufsize");
-  union Tbuf
-    {
-    array<double, bufsize> s;
-    array<Tv, nvec> v;
-    };
-  Tbuf l1ladder, c1v, c2v, newfacv;
-
-  double c1=0x1p+1000;
-  double oldfac=0.;
-  for (size_t m=0; m<bufsize; ++m)
-    l1ladder.s[m] = m;
-
-  MR_assert(res.shape(0)==size_t(ncoef), "bad size of result array");
-
-  int i=0;
-  res(i) = srtiny;
-  double sumfor = (2.*l1min+1.) * res(i)*res(i);
-
-  // do first iteration separately
-  if (i+1<ncoef)
-    {
-    ++i;
-
-    const double l1 = l1min+i;
-    const double l1sq = l1*l1,
-                 c1old = abs(c1),
-                 newfac = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
-    c1 = (l1>1.000001) ? (2.*l1-1.)*(pre2-(l1sq-l1)*m3mm2)/((l1-1.)*newfac)
-                       : -(2.*l1-1.)*l1*(m3mm2)/newfac;
-    res(i) = res(i-1)*c1;
-  
-    oldfac=newfac;
-  
-    sumfor += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=0; k<=i; ++k)
-        res(k)*=srtiny;
-      sumfor*=tiny;
-      }
-    }
-
-  while(true)
-    {
-// prepare buffers
-    for (size_t m=0; m<nvec; ++m)
-      {
-      auto l1v = double(l1min+i+1)+l1ladder.v[m];
-      auto l1sq = l1v*l1v;
-      newfacv.v[m] = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
-      auto tmp1 = native_simd<double>(1.)/((l1v-1.)*newfacv.v[m]);
-      c1v.v[m] = (2.*l1v-1.)*(pre2-(l1sq-l1v)*m3mm2) * tmp1;
-      c2v.v[m] = l1v*tmp1;
-      }
-int ilim = min(ncoef, i+1+int(bufsize));
-int vidx=0;
-while(i+1<ilim)
-  {
-//    if (i+1==ncoef) break; /* all done */
-    ++i;
-    const double l1 = l1min+i,
-                 c1old = abs(c1);
-    c1 = c1v.s[vidx];
-    res(i) = res(i-1)*c1 - res(i-2)*c2v.s[vidx]*oldfac;
-
-    oldfac=newfacv.s[vidx];
-
-    sumfor += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=0; k<=i; ++k)
-        res(k)*=srtiny;
-      sumfor*=tiny;
-      }
-    if (c1old<=abs(c1)) goto bailout_fwd;
-    ++vidx;
-  }
-if (i+1>=ncoef) goto bailout_fwd;
-  }
-bailout_fwd:
-//exit(1);
-  double sumbac=0.;
-  bool last_coeff_is_negative=false;
-  double fct_fwd=1., fct_bwd=1.;
-  int nstep2=ncoef;
-
-  if (i+1<ncoef) /* we have to iterate from the other side */
-    {
-    const double x1=res(i-2), x2=res(i-1), x3=res(i);
-    nstep2 = i-2;
-
-    i=ncoef-1;
-    res(i) = srtiny;
-    sumbac = (2.*l1max+1.) * res(i)*res(i);
-
-    {
-    --i;
-
-    const double l1 = l1min+i,
-                 l1p1sq = (l1+1.)*(l1+1.),
-                 newfac = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-
-    res(i) = res(i+1)*(2.*l1+3.)*(pre2-(l1p1sq+l1+1.)*m3mm2)
-             /((l1+2.)*newfac);
-
-    oldfac=newfac;
-
-    sumbac += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=i; k<ncoef; ++k)
-        res(k)*=srtiny;
-      sumbac*=tiny;
-      }
-    }
-
-#if 1
-  for (size_t m=0; m<bufsize; ++m)
-    l1ladder.s[m] = -int(m);
-
-  while(true)
-    {
-        //auto l1p1 = l1min+i+l1ofs+1.;
-        //auto l1p1sq = l1p1*l1p1;
-        //newfacv = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-        //auto tmp1 = native_simd<double>(1.)/((l1p1+1.)*newfacv);
-        //c1v = (2.*l1p1+1.)*(pre2-(l1p1sq+l1p1)*m3mm2) * tmp1;
-        //c2v = l1p1*tmp1;
-// prepare buffers
-    for (size_t m=0; m<nvec; ++m)
-      {
-      auto l1p1v = double(l1min+i)+l1ladder.v[m];
-      auto l1p1sq = l1p1v*l1p1v;
-      newfacv.v[m] = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-      auto tmp1 = native_simd<double>(1.)/((l1p1v+1.)*newfacv.v[m]);
-      c1v.v[m] = (2.*l1p1v+1.)*(pre2-(l1p1sq+l1p1v)*m3mm2) * tmp1;
-      c2v.v[m] = l1p1v*tmp1;
-      }
-
-  int ilim = max(nstep2, i-int(bufsize));
-  int vidx=0;
-  while(i>ilim)
-    {
-    --i;
-    const double l1 = l1min+i;
-    res(i) = res(i+1)*c1v.s[vidx] - res(i+2)*c2v.s[vidx]*oldfac;
-    oldfac=newfacv.s[vidx];
-
-    sumbac += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=i; k<ncoef; ++k)
-        res(k)*=srtiny;
-      sumbac*=tiny;
-      }
-    ++vidx;
-    }
-  if (i<=nstep2) goto bailout_bwd;
-  }
-bailout_bwd:
-#else
-    do
-      {
-      --i;
-
-      const double l1 = l1min+i,
-                   l1p1sq = (l1+1.)*(l1+1.),
-                   newfac = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-
-      res(i) = (res(i+1) * (2.*l1+3.)*(pre2-(l1p1sq+l1+1.)*m3mm2)
-               -res(i+2) * (l1+1.)*oldfac)
-               / ((l1+2.)*newfac);
-
-      oldfac=newfac;
-
-      sumbac += (2.*l1+1.)*res(i)*res(i);
-      if (abs(res(i))>=srhuge)
-        {
-        for (int k=i; k<ncoef; ++k)
-          res(k)*=srtiny;
-        sumbac*=tiny;
-        }
-      }
-    while (i>nstep2);
-#endif
-
-    for (size_t i=nstep2; i<min(ncoef,nstep2+3); ++i)
-      {
-      auto l1=l1min+i;
-      sumbac -= (2.*l1+1.)*res(i)*res(i);
-      }
-
-    const double ratio = (x1*res(i)+x2*res(i+1)+x3*res(i+2))
-                         /(x1*x1+x2*x2+x3*x3);
-    if (abs(ratio)<1.)
-      { fct_bwd = 1./ratio; sumbac/=ratio*ratio; last_coeff_is_negative=ratio<0; }
-    else
-      { fct_fwd = ratio; sumfor*=ratio*ratio; }
-    }
-  else
-    {
-    last_coeff_is_negative = res(ncoef-1)<0.;
-    }
-
-  double cnorm=1./sqrt(sumfor+sumbac);
-  // follow sign convention: sign(f(l_max)) = (-1)**(l2-l3+m2+m3)
-  bool last_coeff_should_be_negative = nearest_int(abs(l2-l3+m2+m3))&1;
-  if (last_coeff_is_negative != last_coeff_should_be_negative)
-    cnorm = -cnorm;
-
-  for (int k=0; k<nstep2; ++k)
-    res(k)*=cnorm*fct_fwd;
-  for (int k=nstep2; k<ncoef; ++k)
-    res(k)*=cnorm*fct_bwd;
-  }
-
-void wigner3j_internal_tweaked (double l2, double l3, double m2, double m3,
-                        double m1, double l1min, double l1max, int ncoef,
-                        vmav<double,1> &res)
-  {
-  if ((m2==0.) && (m3==0.))
-    return wigner3j_00_internal (l2, l3, l1min, l1max, ncoef, res);
-
-  MR_assert(res.shape(0)==size_t(ncoef), "bad size of result array");
-
-  constexpr double srhuge=0x1p+250, tiny=0x1p-500, srtiny=0x1p-250;
-
-  const double l2ml3sq = (l2-l3)*(l2-l3),
-               pre1 = (l2+l3+1.)*(l2+l3+1.),
-               m1sq = m1*m1,
-               pre2 = m1*(l2*(l2+1.)-l3*(l3+1.)),
-               m3mm2 = m3-m2;
-
-  int i=0;
-  double c1=0x1p+1000;
-  double oldfac=0.;
-
-  res(i) = srtiny;
-  double sumfor = (2.*l1min+1.) * res(i)*res(i);
-
-  double fct_fwd=1, fct_bwd=1;
-  size_t vidx=0;
-  constexpr size_t vlen = native_simd<double>::size();
-  native_simd<double> l1ofs, c1v, c2v, newfacv;
-
-  for (size_t ii=0; ii<vlen; ++ii)
-    l1ofs[ii] = ii;
-
-  // do first iteration separately
-  if (i+1<ncoef)
-    {
-    ++i;
-
-    const double l1 = l1min+i;
-    const double l1sq = l1*l1,
-                 c1old = abs(c1),
-                 newfac = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
-    c1 = (l1>1.000001) ? (2.*l1-1.)*(pre2-(l1sq-l1)*m3mm2)/((l1-1.)*newfac)
-                       : -(2.*l1-1.)*l1*(m3mm2)/newfac;
-    res(i) = res(i-1)*c1;
-  
-    oldfac=newfac;
-  
-    sumfor += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=0; k<=i; ++k)
-        res(k)*=srtiny;
-      sumfor*=tiny;
-      }
-    }
-
-  while(true)
-    {
-    if (i+1==ncoef) break; /* all done */
-    ++i;
-
-    const double l1 = l1min+i,
-                 c1old = abs(c1);
-    if (vidx==0)
-      {
-      auto l1 = l1min+i+l1ofs;
-      auto l1sq = l1*l1;
-      newfacv = sqrt((l1sq-l2ml3sq)*(pre1-l1sq)*(l1sq-m1sq));
-      auto tmp1 = native_simd<double>(1.)/((l1-1.)*newfacv);
-      c1v = (2.*l1-1.)*(pre2-(l1sq-l1)*m3mm2) * tmp1;
-      c2v = l1*tmp1;
-      }
-    c1 = c1v[vidx];
-    double newfac=newfacv[vidx];
-
-    res(i) = res(i-1)*c1v[vidx] - res(i-2)*c2v[vidx]*oldfac;
-
-    oldfac=newfac;
-
-    sumfor += (2.*l1+1.)*res(i)*res(i);
-    if (abs(res(i))>=srhuge)
-      {
-      for (int k=0; k<=i; ++k)
-        res(k)*=srtiny;
-      sumfor*=tiny;
-      }
-    if (c1old<=abs(c1)) break;
-
-    vidx = (vidx+1)%vlen;
-    }
-
-  double sumbac=0.;
-bool last_negative;
-  int nstep2=ncoef;
-  if (i+1<ncoef) /* we have to iterate from the other side */
-    {
-    last_negative = false;
-    for (size_t ii=0; ii<vlen; ++ii)
-      l1ofs[ii] = -double(ii);
-
-    vidx=0;
-    const double x1=res(i-2), x2=res(i-1), x3=res(i);
-    nstep2 = i-2;
-
-    i=ncoef-1;
-    res(i) = srtiny;
-    sumbac = (2.*l1max+1.) * res(i)*res(i);
-
-    do
-      {
-      --i;
-      const double l1 = l1min+i;
-
-      if (vidx==0)
-        {
-        auto l1p1 = l1min+i+l1ofs+1.;
-        auto l1p1sq = l1p1*l1p1;
-        newfacv = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-        auto tmp1 = native_simd<double>(1.)/((l1p1+1.)*newfacv);
-        c1v = (2.*l1p1+1.)*(pre2-(l1p1sq+l1p1)*m3mm2) * tmp1;
-        c2v = l1p1*tmp1;
-        }
-
-      if (i<ncoef-2)
-        res(i) = res(i+1)*c1v[vidx] - res(i+2)*c2v[vidx]*oldfac;
-      else
-        {
-        const double l1p1sq = (l1+1.)*(l1+1.),
-                     newfac = sqrt((l1p1sq-l2ml3sq)*(pre1-l1p1sq)*(l1p1sq-m1sq));
-        res(i) = res(i+1)*(2.*l1+3.)*(pre2-(l1p1sq+l1+1.)*m3mm2)
-                 /((l1+2.)*newfac);
-        }
-      oldfac=newfacv[vidx];
-
-      sumbac += (2.*l1+1.)*res(i)*res(i);
-      if (abs(res(i))>=srhuge)
-        {
-        for (int k=i; k<ncoef; ++k)
-          res(k)*=srtiny;
-        sumbac*=tiny;
-        }
-
-      vidx = (vidx+1)%vlen;
-      }
-    while (i>nstep2);
-
-  for (size_t i=nstep2; i<min(ncoef,nstep2+3); ++i)
-    {
-    auto l1=l1min+i;
-    sumbac -= (2.*l1+1.)*res(i)*res(i);
-    }
-
-    double ratio = (x1*res(i)+x2*res(i+1)+x3*res(i+2))
-                         /(x1*x1+x2*x2+x3*x3);
-    if (abs(ratio)<1.)
-      { fct_bwd = 1./ratio; sumbac/=ratio*ratio; last_negative=ratio<0; }
-    else
-      { fct_fwd = ratio; sumfor*=ratio*ratio; }
-    }
-  else
-    {
-    last_negative = res(ncoef-1)<0.;
-    }
-
-  double cnorm=1./sqrt(sumfor+sumbac);
-  bool last_should_be_negative = nearest_int(l2-l3+m2+m3)&1;
-  if (last_negative != last_should_be_negative)
-    cnorm = -cnorm;
-
-  for (int k=0; k<nstep2; ++k)
-    res(k)*=cnorm*fct_fwd;
-  for (int k=nstep2; k<ncoef; ++k)
-    res(k)*=cnorm*fct_bwd;
-  }
 //Returns all non-zero wigner-3j symbols
 // il2 (in) : l2
 // il3 (in) : l3
@@ -1053,13 +703,6 @@ void wigner3j_namaster (double l2, double l3, double m2, double m3, vector<doubl
   res.resize(ncoef);
   int l1min2, l1max2;
   drc3jj (l2, l3, m2, m3, &l1min2, &l1max2, res.data(), ncoef);
-  }
-void wigner3j_tweaked (double l2, double l3, double m2, double m3, vector<double> &res)
-  {
-  auto [m1, l1min, l1max, ncoef] = wigner3j_checks_and_sizes(l2, l3, m2, m3);
-  res.resize(ncoef);
-  vmav<double,1> tmp(res.data(), {size_t(ncoef)});
-  wigner3j_internal_block<16> (l2, l3, m2, m3, m1, l1min, l1max, ncoef, tmp);
   }
 
 void wigner3j_int (int l2, int l3, int m2, int m3, int &l1min_, vmav<double,1> &res)
