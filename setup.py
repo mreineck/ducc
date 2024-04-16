1923757c83f7f56c50fa95bdd7e8843d53172190
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-04-16 11:28:06 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension
 import pybind11
 
 pkgname = 'ducc0'
-version = '0.33.0'
+version = '0.34.0'
 
 tmp = os.getenv("DUCC0_CFLAGS", "").split(" ")
 user_cflags = [x for x in tmp if x != ""]
