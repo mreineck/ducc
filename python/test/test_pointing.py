20afbadf5d03e8371087a59a1144bec3184b9e73
Martin Reinecke <martin@mpa-garching.mpg.de>
2024-05-09 20:44:02 +0200

    add test

--- python/test/test_pointing.py
+++ python/test/test_pointing.py
@@ -80,3 +80,29 @@ def testp2():
     squat3 = r3.as_quat()
     squat3 *= np.sign(squat3[:, 0]).reshape((-1, 1))
     assert_allclose(ducc0.misc.l2error(quat4, squat3), 0, atol=1e-13)
+
+
+def test_quat_euler():
+    nsamp = 10000
+
+    euler = np.empty((nsamp,3))
+
+    # theta
+    euler[:,0] = np.random.uniform(low=-3*np.pi, high=5*np.pi, size=nsamp)
+    # phi
+    euler[:,1] = np.random.uniform(low=-5*np.pi, high=8*np.pi, size=nsamp)
+    # psi
+    euler[:,2] = np.random.uniform(low=-5*np.pi, high=8*np.pi, size=nsamp)
+
+    euler2 = ducc0.misc.quat2ptg(ducc0.misc.ptg2quat(euler))
+
+    # Normalize angle ranges
+    euler = (euler + 8*np.pi)%(2*np.pi)
+    mask = euler[:,0]>np.pi
+    euler[:,0] = np.where(mask,2*np.pi-euler[:,0],euler[:,0])
+    euler[:,1] = np.where(mask,(euler[:,1]+np.pi)%(2*np.pi),euler[:,1])
+    euler[:,2] = np.where(mask,(euler[:,2]+np.pi)%(2*np.pi),euler[:,2])
+
+    euler2[:,1:3] = (euler2[:,1:3] + 8*np.pi)%(2*np.pi)
+
+    assert_allclose(ducc0.misc.l2error(euler, euler2), 0, atol=1e-13)
