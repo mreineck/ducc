fa2815820b2b8ac11bd49063f430535574eb0dcc
Martin Reinecke <martin@mpa-garching.mpg.de>
2025-10-16 17:05:03 +0200

    add workaround flag fpr Alpine

--- src/ducc0/nufft/spreadinterp_impl.h
+++ src/ducc0/nufft/spreadinterp_impl.h
@@ -1371,8 +1371,11 @@ template<typename Tcalc, typename Tacc, typename Tcoord,typename Tidx> class Spr
           for (size_t cw=0; cw<SUPP; ++cw)
             xdata.c[cw]=kw[cw]*v;
           Tacc * DUCC0_RESTRICT fptr2=reinterpret_cast<Tacc *>(hlp.p0);
+// This didn't work with Alpine Linux at release 0.39.
+// I suspect a compiler bug, but I'm not absolutely sure.
+#ifndef DUCC0_ALPINE_LINUX_WORKAROUND
 // this is quite voodoo, but helps a lot, at least on my machine
-if constexpr(false)//(SUPP<=8)
+if constexpr(SUPP<=8)
   {
           const Tsimd * DUCC0_RESTRICT fptr1=xdata.v.data();
           for (size_t cu=0; cu<SUPP; ++cu)
@@ -1388,6 +1391,7 @@ if constexpr(false)//(SUPP<=8)
               }
   }
 else
+#endif
   {
           const Tacc * DUCC0_RESTRICT fptr1=xdata.f.data();
           const auto j1 = 2*ljump;
