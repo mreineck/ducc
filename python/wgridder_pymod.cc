697ce7927e71d3deea45344a6dbfb8c3ff928173
Martin Reinecke <martin@mpa-garching.mpg.de>
2026-07-11 13:38:39 +0200

    cleanups; remove the 'tuning' wgridder interface

--- python/wgridder_pymod.cc
+++ python/wgridder_pymod.cc
@@ -30,125 +30,6 @@ namespace detail_pymodule_wgridder {
 
 using namespace std;
 
-template<typename T> static NpArr Py2_vis2dirty_tuning(const CNpArr &uvw_,
-  const CNpArr &freq_, const CNpArr &vis_, const OptCNpArr &wgt_, const OptCNpArr &mask_,
-  size_t npix_x, size_t npix_y, double pixsize_x, double pixsize_y,
-  double epsilon, bool do_wgridding, size_t nthreads, size_t verbosity,
-  bool flip_u, bool flip_v, bool flip_w, bool divide_by_n, const OptNpArr &dirty_, double sigma_min,
-  double sigma_max, double center_x, double center_y,
-  bool double_precision_accumulation)
-  {
-  auto uvw = to_cmav<double,2>(uvw_);
-  auto freq = to_cmav<double,1>(freq_);
-  auto vis = to_cmav<complex<T>,2>(vis_);
-  auto wgt = get_OptCNpArr<T>(wgt_, {vis.shape(0),vis.shape(1)});
-  auto wgt2 = to_cmav<T,2>(wgt);
-  auto mask = get_OptCNpArr<uint8_t>(mask_, {uvw.shape(0),freq.shape(0)});
-  auto mask2 = to_cmav<uint8_t,2>(mask);
-  // sizes must be either both zero or both nonzero
-  MR_assert((npix_x==0)==(npix_y==0), "inconsistent dirty image dimensions");
-  auto dirty = (npix_x==0) ? dirty_.value()
-                           : get_OptNpArr<T>(dirty_, {npix_x, npix_y});
-  auto dirty2 = to_vmav<T,2>(dirty);
-  {
-  py::gil_scoped_release release;
-  double_precision_accumulation ?
-    ms2dirty_tuning<T,double>(uvw,freq,vis,wgt2,mask2,pixsize_x,pixsize_y,epsilon,
-      do_wgridding,nthreads,dirty2,verbosity,flip_u,flip_v,flip_w,divide_by_n, sigma_min,
-      sigma_max, center_x, center_y) :
-    ms2dirty_tuning<T,T>(uvw,freq,vis,wgt2,mask2,pixsize_x,pixsize_y,epsilon,
-      do_wgridding,nthreads,dirty2,verbosity,flip_u,flip_v,flip_w,divide_by_n, sigma_min,
-      sigma_max, center_x, center_y);
-  }
-  return dirty;
-  }
-NpArr Py_vis2dirty_tuning(const CNpArr &uvw,
-  const CNpArr &freq, const CNpArr &vis, const OptCNpArr &wgt,
-  size_t npix_x, size_t npix_y, double pixsize_x, double pixsize_y,
-  double epsilon, bool do_wgridding, size_t nthreads,
-  size_t verbosity, const OptCNpArr &mask, bool flip_u, bool flip_v, bool flip_w, bool divide_by_n,
-  const OptNpArr &dirty, double sigma_min, double sigma_max,
-  double center_x, double center_y,
-  bool double_precision_accumulation)
-  {
-  if (isPyarr<complex<float>>(vis))
-    return Py2_vis2dirty_tuning<float>(uvw, freq, vis, wgt, mask, npix_x, npix_y,
-      pixsize_x, pixsize_y, epsilon, do_wgridding, nthreads, verbosity,
-      flip_u, flip_v, flip_w, divide_by_n, dirty, sigma_min, sigma_max, center_x, center_y,
-      double_precision_accumulation);
-  if (isPyarr<complex<double>>(vis))
-    return Py2_vis2dirty_tuning<double>(uvw, freq, vis, wgt, mask, npix_x, npix_y,
-      pixsize_x, pixsize_y, epsilon, do_wgridding, nthreads, verbosity,
-      flip_u, flip_v, flip_w, divide_by_n, dirty, sigma_min, sigma_max, center_x, center_y,
-      double_precision_accumulation);
-  MR_fail("type matching failed: 'vis' has neither type 'c8' nor 'c16'");
-  }
-constexpr auto vis2dirty_tuning_DS = R"""(
-Converts visibilities to a dirty image.
-
-This implementation tries to subdivide the operation into smaller tasks to
-optimize performance.
-
-Parameters
-----------
-uvw: numpy.ndarray((nrows, 3), dtype=numpy.float64)
-    UVW coordinates from the measurement set
-freq: numpy.ndarray((nchan,), dtype=numpy.float64)
-    channel frequencies
-vis: numpy.ndarray((nrows, nchan), dtype=numpy.complex64 or numpy.complex128)
-    the input visibilities.
-    Its data type determines the precision in which the calculation is carried
-    out.
-wgt: numpy.ndarray((nrows, nchan), float with same precision as `vis`), optional
-    If present, its values are multiplied to the input before gridding
-mask: numpy.ndarray((nrows, nchan), dtype=numpy.uint8), optional
-    If present, only visibilities are processed for which mask!=0
-npix_x, npix_y: int
-    dimensions of the dirty image (must both be at least 32)
-    If the `dirty` argument is provided, image dimensions will be inferred from
-    the passed array; in this case npix_x and npix_y must be either consistent
-    with these dimensions, or be zero.
-pixsize_x, pixsize_y: float
-    angular pixel size (in projected radians) of the dirty image
-center_x, center_y: float
-    center of the dirty image relative to the phase center
-    (in projected radians)
-epsilon: float
-    accuracy at which the computation should be done. Must be larger than 2e-13.
-    If `vis` has type numpy.complex64, it must be larger than 1e-5.
-do_wgridding: bool
-    if True, the full w-gridding algorithm is carried out, otherwise
-    the w values are assumed to be zero.
-flip_[uvw]: bool
-    if True, all [uvw] coordinates in uvw are multiplied by -1
-divide_by_n: bool
-    if True, the dirty image pixels are divided by n
-sigma_min, sigma_max: float
-    minimum and maximum allowed oversampling factors
-nthreads: int
-    number of threads to use for the calculation
-verbosity: int
-    0: no output
-    1: some diagnostic output and timings
-dirty: numpy.ndarray((npix_x, npix_y), dtype=float of same precision as `vis`),
-    optional
-    If provided, the dirty image will be written to this array and a handle
-    to it will be returned.
-double_precision_accumulation: bool
-    If True, always use double precision for accumulating operations onto the
-    uv grid. This is necessary to reduce numerical errors in special cases.
-
-Returns
--------
-numpy.ndarray((npix_x, npix_y), dtype=float of same precision as `vis`)
-    the dirty image
-
-Notes
------
-The input arrays should be contiguous and in C memory order.
-Other strides will work, but can degrade performance significantly.
-)""";
-
 template<typename T> static NpArr Py2_vis2dirty(const CNpArr &uvw_,
   const CNpArr &freq_, const CNpArr &vis_, const OptCNpArr &wgt_, const OptCNpArr &mask_,
   size_t npix_x, size_t npix_y, double pixsize_x, double pixsize_y,
@@ -390,103 +271,6 @@ The input arrays should be contiguous and in C memory order.
 Other strides will work, but can degrade performance significantly.
 )""";
 
-template<typename T> static NpArr Py2_dirty2vis_tuning(const CNpArr &uvw_,
-  const CNpArr &freq_, const CNpArr &dirty_, const OptCNpArr &wgt_, const OptCNpArr &mask_,
-  double pixsize_x, double pixsize_y, double epsilon, bool do_wgridding,
-  size_t nthreads, size_t verbosity, bool flip_u, bool flip_v, bool flip_w, bool divide_by_n,
-  const OptNpArr &vis_, double sigma_min, double sigma_max, double center_x, double center_y)
-  {
-  auto uvw = to_cmav<double,2>(uvw_);
-  auto freq = to_cmav<double,1>(freq_);
-  auto dirty = to_cmav<T,2>(dirty_);
-  auto wgt = get_OptCNpArr<T>(wgt_, {uvw.shape(0),freq.shape(0)});
-  auto wgt2 = to_cmav<T,2>(wgt);
-  auto mask = get_OptCNpArr<uint8_t>(mask_, {uvw.shape(0),freq.shape(0)});
-  auto mask2 = to_cmav<uint8_t,2>(mask);
-  auto [vis, vis2] = get_OptNpArr_and_vmav<complex<T>,2>(vis_, {uvw.shape(0),freq.shape(0)});
-  {
-  py::gil_scoped_release release;
-  dirty2ms_tuning<T,T>(uvw,freq,dirty,wgt2,mask2,pixsize_x,pixsize_y,epsilon,
-    do_wgridding,nthreads,vis2,verbosity,flip_u,flip_v,flip_w,divide_by_n, sigma_min,
-    sigma_max, center_x, center_y);
-  }
-  return vis;
-  }
-NpArr Py_dirty2vis_tuning(const CNpArr &uvw,
-  const CNpArr &freq, const CNpArr &dirty, const OptCNpArr &wgt,
-  double pixsize_x, double pixsize_y, double epsilon, bool do_wgridding,
-  size_t nthreads, size_t verbosity, const OptCNpArr &mask,
-  bool flip_u, bool flip_v, bool flip_w, bool divide_by_n, const OptNpArr &vis, double sigma_min,
-  double sigma_max, double center_x, double center_y)
-  {
-  if (isPyarr<float>(dirty))
-    return Py2_dirty2vis_tuning<float>(uvw, freq, dirty, wgt, mask,
-      pixsize_x, pixsize_y, epsilon, do_wgridding, nthreads, verbosity,
-      flip_u, flip_v, flip_w, divide_by_n, vis, sigma_min, sigma_max, center_x, center_y);
-  if (isPyarr<double>(dirty))
-    return Py2_dirty2vis_tuning<double>(uvw, freq, dirty, wgt, mask,
-      pixsize_x, pixsize_y, epsilon, do_wgridding, nthreads, verbosity,
-      flip_u, flip_v, flip_w, divide_by_n, vis, sigma_min, sigma_max, center_x, center_y);
-  MR_fail("type matching failed: 'dirty' has neither type 'f4' nor 'f8'");
-  }
-constexpr auto dirty2vis_tuning_DS = R"""(
-Converts a dirty image to visibilities.
-
-This implementation tries to subdivide the operation into smaller tasks to
-optimize performance.
-
-Parameters
-----------
-uvw: numpy.ndarray((nrows, 3), dtype=numpy.float64)
-    UVW coordinates from the measurement set
-freq: numpy.ndarray((nchan,), dtype=numpy.float64)
-    channel frequencies
-dirty: numpy.ndarray((npix_x, npix_y), dtype=numpy.float32 or numpy.float64)
-    dirty image
-    Its data type determines the precision in which the calculation is carried
-    out.
-    Both dimensions must be least 32.
-wgt: numpy.ndarray((nrows, nchan), same dtype as `dirty`), optional
-    If present, its values are multiplied to the output
-mask: numpy.ndarray((nrows, nchan), dtype=numpy.uint8), optional
-    If present, only visibilities are processed for which mask!=0
-pixsize_x, pixsize_y: float
-    angular pixel size (in projected radians) of the dirty image
-center_x, center_y: float
-    center of the dirty image relative to the phase center
-    (in projected radians)
-epsilon: float
-    accuracy at which the computation should be done. Must be larger than 2e-13.
-    If `dirty` has type numpy.float32, it must be larger than 1e-5.
-do_wgridding: bool
-    if True, the full w-gridding algorithm is carried out, otherwise
-    the w values are assumed to be zero.
-flip_[uvw]: bool
-    if True, all [uvw] coordinates in uvw are multiplied by -1
-divide_by_n: bool
-    if True, the dirty image pixels are divided by n
-sigma_min, sigma_max: float
-    minimum and maximum allowed oversampling factors
-nthreads: int
-    number of threads to use for the calculation
-verbosity: int
-    0: no output
-    1: some diagnostic output and timings
-vis: numpy.ndarray((nrows, nchan), dtype=complex of same precision as `dirty`),
-    optional
-    If provided, the computed visibilities will be stored in this array, and
-    a handle to it will be returned.
-
-Returns
--------
-numpy.ndarray((nrows, nchan), dtype=complex of same precision as `dirty`)
-    the computed visibilities.
-
-Notes
------
-The input arrays should be contiguous and in C memory order.
-Other strides will work, but can degrade performance significantly.
-)""";
 template<typename T> static NpArr Py2_dirty2vis(const CNpArr &uvw_,
   const CNpArr &freq_, const CNpArr &dirty_, const OptCNpArr &wgt_, const OptCNpArr &mask_,
   double pixsize_x, double pixsize_y, double epsilon, bool do_wgridding,
@@ -842,19 +626,6 @@ void add_wgridder(py::module_ &msup)
   add_pythonfuncs(m);
   add_pythonfuncs(m2);
 
-  m2.def("vis2dirty_tuning", &Py_vis2dirty_tuning, vis2dirty_tuning_DS, py::kw_only(), "uvw"_a, "freq"_a, "vis"_a,
-    "wgt"_a=None, "npix_x"_a=0, "npix_y"_a=0, "pixsize_x"_a, "pixsize_y"_a,
-    "epsilon"_a, "do_wgridding"_a=false, "nthreads"_a=1, "verbosity"_a=0,
-    "mask"_a=None, "flip_u"_a=false, "flip_v"_a=false, "flip_w"_a=false,
-    "divide_by_n"_a=true, "dirty"_a=None,
-    "sigma_min"_a=1.1, "sigma_max"_a=2.6, "center_x"_a=0., "center_y"_a=0.,
-    "double_precision_accumulation"_a=false);
-  m2.def("dirty2vis_tuning", &Py_dirty2vis_tuning, dirty2vis_tuning_DS, py::kw_only(), "uvw"_a, "freq"_a, "dirty"_a,
-    "wgt"_a=None, "pixsize_x"_a, "pixsize_y"_a, "epsilon"_a,
-    "do_wgridding"_a=false, "nthreads"_a=1, "verbosity"_a=0, "mask"_a=None,
-    "flip_u"_a=false, "flip_v"_a=false, "flip_w"_a=false, "divide_by_n"_a=true, "vis"_a=None, "sigma_min"_a=1.1,
-    "sigma_max"_a=2.6, "center_x"_a=0., "center_y"_a=0.);
-
   m.def("ms2dirty", &Py_ms2dirty, ms2dirty_DS, "uvw"_a, "freq"_a, "ms"_a,
     "wgt"_a=None, "npix_x"_a, "npix_y"_a, "pixsize_x"_a, "pixsize_y"_a, "nu"_a=0, "nv"_a=0,
     "epsilon"_a, "do_wstacking"_a=false, "nthreads"_a=1, "verbosity"_a=0, "mask"_a=None,
@@ -877,7 +648,7 @@ void add_wgridder(py::module_ &msup)
     "flip_u"_a=false, "flip_v"_a=false, "flip_w"_a=false, "divide_by_n"_a=true, "vis"_a=None, "sigma_min"_a=1.1,
     "sigma_max"_a=2.6, "center_x"_a=0., "center_y"_a=0., "allow_nshift"_a=true);
   }
- 
+
 }
 
 using detail_pymodule_wgridder::add_wgridder;
--- src/ducc0/wgridder/wgridder.h
+++ src/ducc0/wgridder/wgridder.h
@@ -14,7 +14,7 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-/* Copyright (C) 2019-2025 Max-Planck-Society
+/* Copyright (C) 2019-2026 Max-Planck-Society
    Author: Martin Reinecke */
 
 #ifndef DUCC0_WGRIDDER_H
@@ -69,49 +69,6 @@ template<typename Tcalc, typename Tacc, typename Tms, typename Timg>
     size_t verbosity, bool flip_u, bool flip_v, bool flip_w, bool divide_by_n,
     double sigma_min, double sigma_max, double center_x, double center_y, bool allow_nshift);
 
-tuple<size_t, size_t, size_t, size_t, double, double>
- get_facet_data(size_t npix_x, size_t npix_y, size_t nfx, size_t nfy, size_t ifx, size_t ify,
-  double pixsize_x, double pixsize_y, double center_x, double center_y);
-
-template<typename Tcalc, typename Tacc, typename Tms, typename Timg, typename Tms_in=cmav<complex<Tms>,2>> void ms2dirty_tuning(const cmav<double,2> &uvw,
-  const cmav<double,1> &freq, const Tms_in &ms,
-  const cmav<Tms,2> &wgt_, const cmav<uint8_t,2> &mask_, double pixsize_x, double pixsize_y, double epsilon,
-  bool do_wgridding, size_t nthreads, const vmav<Timg,2> &dirty, size_t verbosity,
-  bool flip_u, bool flip_v, bool flip_w, bool divide_by_n, double sigma_min,
-  double sigma_max, double center_x, double center_y);
-
-template<typename Tcalc, typename Tacc, typename Tms, typename Tms_in=cmav<complex<Tms>,1>, typename Timg>
-  void ms2dirty_bda_tuning(
-    const cmav<double,2> &uvw,                        // (nrows,3)
-    const cmav<size_t,1> &freqlist_id,                // (nrows),
-    const cmav<size_t,1> &freqlist_nfreqs,            // (max(freqlist_id)+1)
-    const cmav<double,1> &freqlist_freqs,             // (sum(freqlist_nfreqs), concatenated frequency lists for all freqlist_ids
-    const Tms_in &ms,                                 // concatenated array of visibilities, shape (sum_i(freqlist_nfreq[freqlist_id[i]))
-    const cmav<Tms,1> &wgt_,                          // same shape as above
-    const cmav<uint8_t,1> &mask_,                     // same shape as above
-    double pixsize_x, double pixsize_y, double epsilon,
-    bool do_wgridding, size_t nthreads, const vmav<Timg,2> &dirty, size_t verbosity,
-    bool flip_u, bool flip_v, bool flip_w, bool divide_by_n, double sigma_min,
-    double sigma_max, double center_x, double center_y);
-
-template<typename Tcalc, typename Tacc, typename Tms, typename Timg> void dirty2ms_tuning(const cmav<double,2> &uvw,
-  const cmav<double,1> &freq, const cmav<Timg,2> &dirty,
-  const cmav<Tms,2> &wgt_, const cmav<uint8_t,2> &mask_, double pixsize_x, double pixsize_y,
-  double epsilon, bool do_wgridding, size_t nthreads, const vmav<complex<Tms>,2> &ms,
-  size_t verbosity, bool flip_u, bool flip_v, bool flip_w, bool divide_by_n,
-  double sigma_min, double sigma_max, double center_x, double center_y);
-
-template<typename Tcalc, typename Tacc, typename Tms, typename Timg>
-  void dirty2ms_bda_tuning(
-    const cmav<double,2> &uvw,
-    const cmav<size_t,1> &freqlist_id,                // (nrows),
-    const cmav<size_t,1> &freqlist_nfreqs,            // (max(freqlist_id)+1)
-    const cmav<double,1> &freqlist_freqs,             // (sum(freqlist_nfreqs), concatenated frequency lists for all freqlist_ids
-    const cmav<Timg,2> &dirty,
-    const cmav<Tms,1> &wgt_, const cmav<uint8_t,1> &mask_, double pixsize_x, double pixsize_y,
-    double epsilon, bool do_wgridding, size_t nthreads, const vmav<complex<Tms>,1> &ms,
-    size_t verbosity, bool flip_u, bool flip_v, bool flip_w, bool divide_by_n,
-    double sigma_min, double sigma_max, double center_x, double center_y);
 } // namespace detail_gridder
 
 // public names
@@ -119,8 +76,6 @@ using detail_gridder::ms2dirty;
 using detail_gridder::dirty2ms;
 using detail_gridder::ms2dirty_bda;
 using detail_gridder::dirty2ms_bda;
-using detail_gridder::ms2dirty_tuning;
-using detail_gridder::dirty2ms_tuning;
 
 } // namespace ducc0
 
