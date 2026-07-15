b886d3336b8c404bb9305320e49dc30b74a7fed7
Martin Reinecke <martin@mpa-garching.mpg.de>
2026-07-15 21:22:17 +0200

    start playing with multiarch packages

--- python/ducc.cc
+++ python/ducc.cc
@@ -3,6 +3,25 @@
 
 using namespace ducc0;
 
+#if defined (DUCC0_TARGET)
+#if defined (__GNUC__)
+static bool cpu_supports(const std::string &feature)
+  {
+  __builtin_cpu_init ();
+  if (feature=="sse2")
+    return __builtin_cpu_supports ("sse2");
+  if (feature=="avx2")
+    return __builtin_cpu_supports ("avx2");
+  if (feature=="avx512f")
+    return __builtin_cpu_supports ("avx512f");
+  return false;
+  }
+#else
+static bool cpu_supports(const char * /*feature*/)
+  { return false; }
+#endif
+#endif
+
 #ifdef DUCC0_USE_NANOBIND
 NB_MODULE(PKGNAME, m)
 #else
@@ -11,6 +30,10 @@ PYBIND11_MODULE(PKGNAME, m)
   {
 #define DUCC0_XSTRINGIFY(s) DUCC0_STRINGIFY(s)
 #define DUCC0_STRINGIFY(s) #s
+#if defined (DUCC0_TARGET)
+  MR_assert(cpu_supports(DUCC0_XSTRINGIFY(DUCC0_TARGET)),
+    "required CPU feature not supported by this CPU");
+#endif
   m.attr("__version__") = DUCC0_XSTRINGIFY(PKGVERSION);
 #undef DUCC0_STRINGIFY
 #undef DUCC0_XSTRINGIFY
